89255cd4bf85a752920c9e0a0ce4a42e4aae121d
fifo1175 <fifo1175@colorado.edu>
2022-02-15 21:44:32 -0700

    index page, user table, user signup

--- src/main/java/com/project/travelPlanner/acessingdatamysql/UserRepository.java
+++ src/main/java/com/project/travelPlanner/acessingdatamysql/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.project.travelPlanner.acessingdatamysql.User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
+// since it extends CrudRepository, it can access CRUD functionality
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
